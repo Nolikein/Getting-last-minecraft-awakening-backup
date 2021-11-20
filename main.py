fed45a374f654bc8499d0655cbf3aeb2da885aa9
Come Wasik <wasik.come@gmail.com>
2021-11-20 12:57:40 +0100

    update main.py - use with to open file to write

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Setup global variables
+
 backupRegistry = "backupstore.txt"
 minecraftWorldName = os.getenv('MINECRAFT_WORLD_NAME')
 localBackupDirectory = os.getenv('OUTPUT_DIRECTORY').rstrip("/") + "/"
@@ -13,15 +14,19 @@ localBackupRegistryPath = localBackupDirectory + backupRegistry
 
 
 def getFile(ftp, fromFilename, toPath):
-    """Get a file from a ftp connexion
-    fromFilename (String) the source filename
-    toPath (String) the destination path (directory and filename)
     """
+    Get a file from the FTP server and save it to the local filesystem.
+    :param ftp: FTP object
+    :param fromFilename: Filename to get from the FTP server
+    :param toPath: Path to save the file to
+    """
+
     print("Begin to copy file " + fromFilename)
     try:
-        ftp.retrbinary("RETR " + fromFilename, open(toPath, 'wb').write)
+        with open(toPath, 'wb') as file:
+            ftp.retrbinary('RETR ' + fromFilename, file.write)
         print("Copied file " + fromFilename)
-    except:
+    except Exception as e:
         print("Can't/cancelled copying file " + fromFilename)
 
 
@@ -29,7 +34,7 @@ def getFile(ftp, fromFilename, toPath):
 with FTP() as ftp:
     ftp.connect(host=os.getenv('FTP_HOST'), port=int(os.getenv('FTP_PORT')))
     ftp.login(user=os.getenv('FTP_USER'), passwd=os.getenv('FTP_PASSWORD'))
-    
+
     # Get backup registry
     print("Go to backupstore directory")
     ftp.cwd('/backups/' + minecraftWorldName)
@@ -37,13 +42,13 @@ with FTP() as ftp:
 
     # Search which backup.zip we need to download
     with open(localBackupRegistryPath, "r") as f:
-        lines=[l.rstrip() for l in f.readlines() if len(l) > 0]
+        lines = [l.rstrip() for l in f.readlines() if len(l) > 0]
         if len(lines) == 0:
             raise Exception("The backupstore.txt file is empty")
-        lastLine=lines[-1]
-        filenameToFetch="Backup-%s-%s-%s-%s--%s-%s.zip" % (
+        lastLine = lines[-1]
+        filenameToFetch = "Backup-%s-%s-%s-%s--%s-%s.zip" % (
             (minecraftWorldName,) + tuple(lastLine.split("=")[1:]))
-        pathToFile="%s/%s/%s" % tuple(lastLine.split("=")[1:4])
+        pathToFile = "%s/%s/%s" % tuple(lastLine.split("=")[1:4])
 
     # Download the backup file
     print("Go to backup directory")
